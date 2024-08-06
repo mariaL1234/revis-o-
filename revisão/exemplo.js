976ca6b245924b8a50bca0fac5dae497f7ac34b2
mariaL1234 <maria_l_camargo@estudante.sesisenai.org.br>
2024-08-06 14:44:18 -0300

    Add files via upload

---
+++ revisão/exemplo.js
@@ -0,0 +1,52 @@
+//manipulaçao de nome
+
+let primeiroNome = "maria"
+let segundoNome = "luiza"
+let nomeCompleto = primeiroNome + " " + segundoNome
+
+console.log(nomeCompleto)
+
+//operação  com numeros
+
+let numero1 = 10
+let numero2 = 20
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
+
+console.log (soma, subtracao, multiplicacao, divisao)
+
+//trabalho com boleanos
+let estaChovendo = false
+if(estaChovendo){
+    console.log("está chivendo")
+}else{
+    console.log("nao  esta chovendo")
+
+}
+
+//uso  de null e undefiened
+
+let resposta = null
+let nota 
+
+console.log(resposta, nota)
+
+//symbol
+
+let idUnico = Symbol()
+let = produto = {
+    [idUnico]: 1234
+}
+console.log(produto[idUnico])
+
+//trabalhando com bigInt
+
+let grandeInteiro = BigInt("6709259537247826409831526703982")
+let numero = 3568231567878241049837
+let numero3 = grandeInteiro
+console.log(grandeInteiro)
+console.log(numero)
+console.log(numero3)
